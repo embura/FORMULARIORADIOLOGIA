c6dba93064e13f0eb0ed5bdaac53c7f584ca8d45
Jefferson S Coelho <embura@gmail.com>
2025-08-19 08:07:28 -0300

    removed TomografiaOdontologica componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import React from 'react';
-import TomografiaOdontologica from './components/TomografiaOdontologica/TomografiaOdontologica';
-import Grid from './components/Grid/Grid'
-
-
-
+import React from "react";
+import TomografiaOdontologica from "./components/TomografiaOdontologica/TomografiaOdontologica";
+import Grid from "./components/Grid/Grid";
 
 import Radiagrafia from "./components/Grid/Radiografia/Radiografia";
 import Tomografia from "./components/Grid/Tomografia/Tomografia";
-import Documentacao from "./components/Grid/Documentacao/Documentacao"
-import Protocolos from "./components/Grid/Protocolos/Protocolos"
-import Servicos from "./components/Grid/Servicos/Servicos"
+import Documentacao from "./components/Grid/Documentacao/Documentacao";
+import Protocolos from "./components/Grid/Protocolos/Protocolos";
+import Servicos from "./components/Grid/Servicos/Servicos";
 const App = () => {
-    return (
-        <div>
-            <TomografiaOdontologica />
-            <Grid
-            tomografia={Tomografia}
-            radiografia={Radiagrafia}
-            documentacao={Documentacao}
-            protocolos={Protocolos}
-            servicos={Servicos}
-              
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Grid
+        tomografia={Tomografia}
+        radiografia={Radiagrafia}
+        documentacao={Documentacao}
+        protocolos={Protocolos}
+        servicos={Servicos}
+      />
+    </div>
+  );
 };
 
 export default App;
